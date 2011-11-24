df248e1ea6f90b626a9fdffad241ecb5d1369b7c
Dao Hoang Son <daohoangson@gmail.com>
2011-11-25 01:13:52 +0700

    Initial Commit

---
+++ RubikTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RubikTest
+//
+//  Created by Son Dao Hoang on 11/25/11.
+//  Copyright (c) 2011 UET. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
